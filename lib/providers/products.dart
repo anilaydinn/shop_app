339eaf106ae2e8cad755f88f2ab08eb791d90bea
Anıl Aydın <anil_aydinn@outlook.com>
2022-11-28 22:24:19 +0300

    [Anil] Add loading spinner for add product

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -51,11 +51,11 @@ class Products with ChangeNotifier {
     return _items.firstWhere((product) => product.id == id);
   }
 
-  void addProduct(Product product) {
+  Future<void> addProduct(Product product) {
     final url = Uri.https(
         'shop-app-f889f-default-rtdb.europe-west1.firebasedatabase.app',
         '/products.json');
-    http
+    return http
         .post(url,
             body: json.encode({
               'title': product.title,
