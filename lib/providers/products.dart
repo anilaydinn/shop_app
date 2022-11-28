4d1a1f50a24ea67277322ba40b281fa033ed0efb
Anıl Aydın <anil_aydinn@outlook.com>
2022-11-28 22:05:41 +0300

    [Anil] Add then callback to add product api call

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -55,23 +55,26 @@ class Products with ChangeNotifier {
     final url = Uri.https(
         'shop-app-f889f-default-rtdb.europe-west1.firebasedatabase.app',
         '/products.json');
-    http.post(url,
-        body: json.encode({
-          'title': product.title,
-          'description': product.description,
-          'imageUrl': product.imageUrl,
-          'price': product.price,
-          'isFavorite': product.isFavorite,
-        }));
-    final newProduct = Product(
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-      id: DateTime.now().toString(),
-    );
-    _items.add(newProduct);
-    notifyListeners();
+    http
+        .post(url,
+            body: json.encode({
+              'title': product.title,
+              'description': product.description,
+              'imageUrl': product.imageUrl,
+              'price': product.price,
+              'isFavorite': product.isFavorite,
+            }))
+        .then((response) {
+      final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        id: json.decode(response.body)['name'],
+      );
+      _items.add(newProduct);
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {
