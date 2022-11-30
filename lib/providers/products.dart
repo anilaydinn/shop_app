7873812a2918e897f73980849df6b1736d47c179
Anıl Aydın <anil_aydinn@outlook.com>
2022-11-30 22:02:43 +0300

    [Anil] Add update product

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -75,9 +75,24 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((product) => product.id == id);
     if (prodIndex >= 0) {
+      final url = Uri.https(
+        'shop-app-f889f-default-rtdb.europe-west1.firebasedatabase.app',
+        '/products/$id.json',
+      );
+      await http.patch(
+        url,
+        body: json.encode(
+          {
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price,
+          },
+        ),
+      );
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
